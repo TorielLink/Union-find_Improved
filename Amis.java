de8ae02f866c0c7312cffebae9dce9605869b00b
Clo <torislink.pub@gmail.com>
2023-10-10 21:12:30 +0200

    Fait : - Base du TD : fonctions find et union
    À faire :
    - Fonctions isolate et add

--- Amis.java
+++ Amis.java
@@ -1,22 +1,28 @@
 public class Amis {
-    private int[] listeAmis;
+    private int[] amis;
     public void initialiser(int n) {
         for (int i = 0; i < n - 1; i++) {
             listeAmis[i] = i;
         }
     }
-    
-    public int trouverRepresentant(int h) {
+
+    public int find(int h) {
         int representant = h;
-        while (representant != listeAmis[representant]) {
-            representant = listeAmis[representant];
+        while (representant != amis[representant]) {
+            representant = amis[representant];
+        }
+        int a = h;
+        while (a != amis[a]) {
+            int temp = a;
+            a = amis[a];
+            amis[temp] = representant;
         }
         return representant;
     }
 
-    public void unionAmis(int h1, int h2) {
-        int representant1 = trouverRepresentant(h1);
-        int representant2 = trouverRepresentant(h2);
-        listeAmis[representant1] = representant2;
+    public void union(int h1, int h2) {
+        int representant1 = find(h1);
+        int representant2 = find(h2);
+        amis[representant1] = representant2;
     }
 }
